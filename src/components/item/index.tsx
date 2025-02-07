68a51f0da306a87e3e042696bd4b9a8a6caa1da5
trshmdo <segovskiy@gmail.com>
2025-02-07 17:46:21 +0300

    changed item comp

--- src/components/item/index.tsx
+++ src/components/item/index.tsx
@@ -2,14 +2,14 @@ import * as styles from "./styles.module.css"
 
 export default function Item() {
   return (
-    <>
-      <div className={styles.container}>
+    <div className={styles.container}>
+      <article>
         <h3>Title of the Task</h3>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
           obcaecati ipsum sit blanditiis dolorem labore, nesciunt
         </p>
-      </div>
-    </>
+      </article>
+    </div>
   )
 }
